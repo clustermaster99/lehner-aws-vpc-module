54d8f47b3dec575e999aea7cc4bb8dbfe5515f6c
clustermaster99 <marc@marclehner.com>
2023-08-10 15:13:40 -0500

    provider

--- main.tf
+++ main.tf
@@ -8,3 +8,14 @@ resource "aws_vpc" "lehner-vpc" {
     "Name" = var.name
   }
 }
+
+terraform {
+  required_version = ">= 0.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72"
+    }
+  }
+}
