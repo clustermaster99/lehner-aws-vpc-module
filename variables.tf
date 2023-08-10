4ebd3f3dc4affa5f98b2d5e1b85624ff87beae6b
clustermaster99 <marc@marclehner.com>
2023-08-10 14:52:12 -0500

    k

--- variables.tf
+++ variables.tf
@@ -11,14 +11,14 @@ variable "name" {
 
 variable "region" {
   description = "region"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 
 variable "cidr" {
   description = "(Optional) The IPv4 CIDR block for the VPC."
-  type = string
+  type        = string
   default     = "10.0.0.0/16"
 }
 
